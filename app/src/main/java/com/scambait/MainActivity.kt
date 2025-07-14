70e9042fe8ada95298473c1e3454b38c8cba6f46
Darius <draghici.ion@gmail.com>
2025-07-14 17:59:11 +0300

    Initial commit

---
+++ app/src/main/java/com/scambait/MainActivity.kt
@@ -0,0 +1,11 @@
+﻿package com.scambait
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
